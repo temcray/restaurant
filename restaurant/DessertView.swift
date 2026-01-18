7480b78aea814ed5dfc05062184256c4d2ece629
Tatiana Mccray <ejamison06@gmail.com>
2026-01-17 19:48:01 -0600

    assignment 2

---
+++ restaurant/DessertView.swift
@@ -0,0 +1,18 @@
+//
+//  DessertView.swift
+//  restaurant
+//
+//  Created by Tatiana6mo on 1/17/26.
+//
+
+import SwiftUI
+
+struct DessertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DessertView()
+}
---
+++ restaurant/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  restaurant
+//
+//  Created by Tatiana6mo on 1/16/26.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
